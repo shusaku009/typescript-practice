c4ef5b921976259bd2c9e572334f6a79b77c8ccb
shusaku009 <shusaku009@gmail.com>
2025-07-05 14:37:21 +0900

    add: テキストボックスの値の取得まで

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 // htmlのidをadd-task-buttonを変数に入れる
 // 見つからない場合は早期リターン
 // 変数が存在している場合は、DOMを追加する
+const addTaskButton = document.getElementById(
+  "add-task-button",
+) as HTMLButtonElement;
+const taskList = document.getElementById("task-list");
+let taskText = document.getElementById("input-text") as HTMLInputElement;
+
 function addTaskList(): void {
-  const addTaskButton = document.getElementById(
-    "add-task-button",
-  ) as HTMLButtonElement;
-  console.log("タスクボタン", addTaskButton);
+  console.log(`タスク：`, taskText.value);
 }
 
 document.addEventListener("click", addTaskList);
