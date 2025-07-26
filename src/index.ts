9326e321ceb808720a3fdebb9bd89ef34ea0d8c5
shusaku009 <shusaku009@gmail.com>
2025-07-26 16:21:55 +0900

    update: タスクを表示させるための要素の取得で、querySelectorでは無くidを使用する

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,17 @@ type Task = {
 const addTaskButton = document.getElementById(
   "add-task-button",
 ) as HTMLButtonElement;
-const taskList = document.querySelector(".taskList") as HTMLHtmlElement;
+const taskList = document.getElementById("addTask") as HTMLHtmlElement;
 let taskText = document.getElementById("input-text") as HTMLInputElement;
 let task: Task[] = [];
 
 function addTask(): void {
   task.push({ id: task.length + 1, title: taskText.value, completed: false });
   taskList.insertAdjacentHTML("afterend", `<p>${taskText.value}</p>`);
+  taskList.insertAdjacentHTML(
+    "afterend",
+    `<div class="flex"><p>${taskText.value}</p><input type="checkbox" name="" value=""></div>`,
+  );
 }
 
 addTaskButton.addEventListener("click", addTaskList);
