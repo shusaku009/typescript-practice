0643cb5471e55db5f62ac13ce6642f7ef7e10c54
shusaku009 <shusaku009@gmail.com>
2025-07-26 15:10:13 +0900

    remove: 不要なコメントを削除

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-// htmlのidをadd-task-buttonを変数に入れる
-// 見つからない場合は早期リターン
-// 変数が存在している場合は、DOMを追加する
 type Task = {
   id: number;
   title: string;
@@ -13,7 +10,6 @@ const taskList = document.getElementById("task-list");
 let taskText = document.getElementById("input-text") as HTMLInputElement;
 let newElement = document.createElement("p");
 let task: Task[] = [];
-// HTMLのbody要素に追加するか、専用のコンテナを作成
 
 function addTaskList(): void {
   const newElement = document.createElement("p");
