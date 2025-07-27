59c1380c6e5d2001481c3ab520dc589876c1cddb
shusaku009 <shusaku009@gmail.com>
2025-07-27 16:21:04 +0900

    add: チェックボックスの状態をJS側で取得する

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,38 @@ type Task = {
   title: string;
   completed: boolean;
 };
+
 const addTaskButton = document.getElementById(
   "add-task-button",
 ) as HTMLButtonElement;
 const taskList = document.getElementById("addTask") as HTMLHtmlElement;
 let taskText = document.getElementById("input-text") as HTMLInputElement;
 let task: Task[] = [];
+const newElement = document.getElementById("complete") as HTMLInputElement;
 
 function addTask(): void {
   task.push({ id: task.length + 1, title: taskText.value, completed: false });
-  taskList.insertAdjacentHTML("afterend", `<p>${taskText.value}</p>`);
   taskList.insertAdjacentHTML(
     "afterend",
-    `<div class="flex"><p>${taskText.value}</p><input type="checkbox" name="" value=""></div>`,
+    `<div class="flex"><p>${taskText.value}</p><input id="complete" type="checkbox" name="" value=""></div>`,
   );
 }
 
-addTaskButton.addEventListener("click", addTaskList);
-//
-// document
-//   .getElementById("add-task-button")
-//   ?.addEventListener("click", addTaskList);
+function completTask(): void {
+  newElement.addEventListener("click", () => {
+    completTask();
+    console.log("チェックボックス", newElement.checked);
+  });
+}
+
+document.body.addEventListener("click", (event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.type === "checkbox") {
+    const checkbox = target as HTMLInputElement;
+    console.log("チェックボックス", checkbox.checked);
+  }
+});
+
+addTaskButton.addEventListener("click", () => {
+  addTask();
+});
